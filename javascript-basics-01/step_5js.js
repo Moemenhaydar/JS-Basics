892e48692ddb7bff3ef15ce88dc0b8a0f153977f
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-07 21:20:15 +0200

    step_5  complete

---
+++ javascript-basics-01/step_5js.js
@@ -0,0 +1,8 @@
+
+var n1=prompt("enter the first nb");
+var n2=prompt("enter the second nb");
+document.getElementById("first_number").value=n1;
+document.getElementById("second_number").value=n2;
+document.getElementById("validate").onclick= function (){
+    alert("The multiple is " + (n1*n2));
+}
