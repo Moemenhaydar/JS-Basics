fd3d32f659f480866bf49979ce682f691929274c
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-07 20:53:42 +0200

    step_4  complete

--- javascript-basics-01/step_4js.js
+++ javascript-basics-01/step_4js.js
@@ -1,2 +1,10 @@
+var name=prompt("what's ur name")
+var last_name=prompt("what's last name ")
+var city=prompt("where do you live")
 
-alert("Hello"+name+last_name+"who live in "+city)
+document.getElementById("name").value=name;
+document.getElementById("surname").value=last_name;
+document.getElementById("city").value=city;
+document.getElementById("validate").onclick=function(){
+    alert("Hello "+name+" "+last_name+" who live in "+city);
+}
