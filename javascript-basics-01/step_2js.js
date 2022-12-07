df862e66bef4be64b7042aa606dc46f09d6cf0dd
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-07 20:34:03 +0200

    step_2  complete

--- javascript-basics-01/step_2js.js
+++ javascript-basics-01/step_2js.js
@@ -1,6 +1,6 @@
-function fun(){
+
     var name =prompt("enter ur name")
     var lname=prompt("enter ur last name")
     var ville=prompt("enter ur ville")
     alert("Name : "+name+ "\nLast name : "+lname+"\nvile : "+ville)
-}
+
