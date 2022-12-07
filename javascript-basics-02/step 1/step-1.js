bec282b3abcf9f78c5db2cc8a5bad2e9bf4560b8
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-07 22:52:10 +0200

    step_1 JS02  complete

---
+++ javascript-basics-02/step 1/step-1.js
@@ -0,0 +1,6 @@
+document.querySelector("img").onmouseover=()=>{
+    document.getElementById("image1").src="images/image1_2.jpg"
+}
+document.querySelector("img").onmouseleave=()=>{
+    document.getElementById("image1").src="images/image1.jpg"
+}
