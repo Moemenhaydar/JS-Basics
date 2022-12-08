5400730fea2d5fe9d47127fb8c3a0b0332fb6653
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-08 22:47:34 +0200

    step_3 JS03  complete

---
+++ javascript-basics-03/step 3/script_3.js
@@ -0,0 +1,7 @@
+var red=document.querySelector(".red");
+var green=document.querySelector(".green");
+var blue=document.querySelector(".blue");
+var par=document.querySelector("#text");
+red.addEventListener("click",()=>par.style.color='red');
+blue.addEventListener("click",()=>par.style.color='blue');
+green.addEventListener("click",()=>par.style.color='green'); 
